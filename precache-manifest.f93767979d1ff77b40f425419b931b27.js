37e0825a9e8b3a4c8bce25c23e4e11c239b9b6f1
freyrh <freyr.helgas@gmail.com>
2020-05-22 23:33:46 +0000

    Updates

---
+++ precache-manifest.f93767979d1ff77b40f425419b931b27.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59062a5e37451fd609b3cd1f6cf2b552",
+    "url": "/samferda-frontend/index.html"
+  },
+  {
+    "revision": "49f3753dc92639e46e8b",
+    "url": "/samferda-frontend/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "da5a8ca04e6ac62eb9df",
+    "url": "/samferda-frontend/static/css/main.8ab46b3f.chunk.css"
+  },
+  {
+    "revision": "49f3753dc92639e46e8b",
+    "url": "/samferda-frontend/static/js/2.6276bf0c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/samferda-frontend/static/js/2.6276bf0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da5a8ca04e6ac62eb9df",
+    "url": "/samferda-frontend/static/js/main.fc2541fd.chunk.js"
+  },
+  {
+    "revision": "3a5b50f74114e5e32b03",
+    "url": "/samferda-frontend/static/js/runtime-main.bb17779e.js"
+  }
+]);
